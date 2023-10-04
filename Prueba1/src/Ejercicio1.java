6c80d357ce8f70baa0543fdf2972679fb7c58b67
Francisco Javier Solana <caniles.11@gmail.com>
2023-10-04 09:54:00 +0200

    cambios

---
+++ Prueba1/src/Ejercicio1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        double a, b, c, resultadoA, resultadoB, resultadoC;
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextDouble();
+        int b = sc.nextDouble();
+        int c = sc.nextDouble();
+
+        resultadoA = a*b + c*((3-a)/(2*b));
+        resultadoB = ((2+a*b)/4)^(c+2);
+        resultadoC =
+    }
+}
